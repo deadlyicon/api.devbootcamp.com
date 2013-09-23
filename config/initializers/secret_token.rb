9755d59cfebe39e6497c16bebaa97afdd3e7262a
Jared Grippe <jared@deadlyicon.com>
2013-09-23 16:44:23 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ApiDevbootcampCom::Application.config.secret_key_base = 'c3cac856163013511653c95281d94531d190638c33c08d0d086ea8ed66732c67a3dd982c75a199f1065022d291c6528c5b91692f81cfd48c5b91dc61f6426e10'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ApiDevbootcampCom::Application.config.session_store :cookie_store, key: '_api_devbootcamp_com_session'
