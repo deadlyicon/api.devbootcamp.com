b6b24cf1f3bf9b173c14a93235ff9a09b6f680b6
Jared Grippe <jared@deadlyicon.com>
2013-09-30 09:21:32 -0700

    WIP

--- lib/dbc/cohort.rb
+++ lib/dbc/cohort.rb
@@ -3,9 +3,25 @@ class Dbc::Cohort < ActiveRecord::Base
   has_many :members, class_name: 'Dbc::User'
   belongs_to :location
 
+  before_validation :generate_slug, :generate_email
+
+  validates_presence_of :name, :email, :slug, :start_date
+  validates_uniqueness_of :name, :email, :slug
+
   def location= location
     location = Dbc::Location.where(name:location).first! if !location.is_a? Dbc::Location
     super location
+  end
+
+  private
+
+  def generate_slug
+    self.slug ||= name.gsub(/\s/,'_')
+  end
+
+  def generate_email
+    return if start_date.blank?
+    self.email ||= "#{name.gsub(/\W+/,'.')}-#{start_date.year}@devbootcamp.com"
   end
 
 end
--- spec/integration/v1/cohorts_spec.rb
+++ spec/integration/v1/cohorts_spec.rb
@@ -23,8 +23,25 @@ describe 'v1 cohorts' do
       expect( cohort["slug"]       ).to eq "purple_nerpals"
       expect( cohort["email"]      ).to eq "purple.nerpals-2013@devbootcamp.com"
       expect( cohort["location"]   ).to eq "San Francisco"
-      expect( cohort["start_date"] ).to eq params["start_date"].as_json
+      expect( cohort["start_date"] ).to eq params["start_date"].to_date.as_json
       expect( cohort["visible"]    ).to eq true
+
+      users = 5.times.map do |i|
+        params = {
+          "name"                  => "new usser #{i}",
+          "email"                 => "new.user.#{1}@gmail.com",
+          "password"              => "ilovecats",
+          "password_confirmation" => "ilovecats",
+          "roles"                 => ["student"],
+          "cohort_id"             => cohort["id"],
+        }
+
+        post '/v1/users', {"user" => params}
+        expect(response).to be_ok
+        response.json
+      end
+
+      # get "/v1/cohorts/#{cohort["id"]}"
 
     end
   end
