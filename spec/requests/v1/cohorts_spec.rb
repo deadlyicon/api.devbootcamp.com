1004d11c78707d79ea4cfe2c3cebe5db600e45ee
Jared Grippe <jared@deadlyicon.com>
2013-09-30 08:11:59 -0700

    WIP

--- spec/requests/v1/cohorts_spec.rb
+++ spec/requests/v1/cohorts_spec.rb
@@ -5,6 +5,19 @@ describe '/v1/cohorts' do
 
   let(:cohort){ Dbc::Cohort.first! }
 
+  def serialize cohort
+    {
+      "id"          => cohort.id.as_json,
+      "name"        => cohort.name.as_json,
+      "location_id" => cohort.location_id.as_json,
+      "in_session"  => cohort.in_session.as_json,
+      "start_date"  => cohort.start_date.as_json,
+      "email"       => cohort.email.as_json,
+      "visible"     => cohort.visible.as_json,
+      "slug"        => cohort.slug.as_json,
+    }
+  end
+
   as_an :admin do
 
     describe 'GET /v1/cohorts' do
@@ -40,24 +53,12 @@ describe '/v1/cohorts' do
     describe "GET /v1/cohorts/:id" do
       it "should render the cohort as json" do
         get "v1/cohorts/#{cohort.id}"
-
-        expect(response.json).to eq(
-          "id"           => cohort.id.as_json,
-          "name"         => cohort.name.as_json,
-          "location_id"  => cohort.location_id.as_json,
-          "in_session"   => cohort.in_session.as_json,
-          "start_date"   => cohort.start_date.as_json,
-          "email"        => cohort.email.as_json,
-          "visible"      => cohort.visible.as_json,
-          "slug"         => cohort.slug.as_json,
-        )
+        expect(response.json).to eq serialize(cohort)
       end
     end
 
     describe "PUT /v1/cohorts/:id" do
-
       it "should update a cohort and render it as json" do
-
         updates = {
           "name"  => "hump dragons",
           "email" => "hump.dragons-2014@devbootcamp.com",
@@ -66,25 +67,11 @@ describe '/v1/cohorts' do
         put "v1/cohorts/#{cohort.id}", "cohort" => updates
 
         expect(response).to be_success
-
         cohort.reload
-
-        expect(response.json).to eq(
-          "id"           => cohort.id.as_json,
-          "name"         => cohort.name.as_json,
-          "location_id"  => cohort.location_id.as_json,
-          "in_session"   => cohort.in_session.as_json,
-          "start_date"   => cohort.start_date.as_json,
-          "email"        => cohort.email.as_json,
-          "visible"      => cohort.visible.as_json,
-          "slug"         => cohort.slug.as_json,
-        )
-
+        expect(response.json).to eq serialize(cohort)
         expect( cohort.name  ).to eq updates["name"].as_json
         expect( cohort.email ).to eq updates["email"].as_json
-
       end
-
     end
 
 
@@ -92,16 +79,7 @@ describe '/v1/cohorts' do
       it "should delete the cohort" do
         delete "/v1/cohorts/#{cohort.id}"
         expect(response.status).to eq 200
-        expect(response.json).to eq(
-          "id"          => cohort.id.as_json,
-          "name"         => cohort.name.as_json,
-          "location_id"  => cohort.location_id.as_json,
-          "in_session"   => cohort.in_session.as_json,
-          "start_date"   => cohort.start_date.as_json,
-          "email"        => cohort.email.as_json,
-          "visible"      => cohort.visible.as_json,
-          "slug"         => cohort.slug.as_json,
-        )
+        expect(response.json).to eq serialize(cohort)
         expect{ cohort.reload }.to raise_error ActiveRecord::RecordNotFound
       end
     end
@@ -110,7 +88,7 @@ describe '/v1/cohorts' do
 
 
 
-  as_an :student do
+  as_a :student do
 
     describe 'GET /v1/cohorts' do
       it "should render all cohorts as json" do
@@ -132,17 +110,54 @@ describe '/v1/cohorts' do
     describe "GET /v1/cohorts/:id" do
       it "should render the cohort as json" do
         get "v1/cohorts/#{cohort.id}"
+        expect(response.json).to eq serialize(cohort)
+      end
+    end
 
-        expect(response.json).to eq(
-          "id"           => cohort.id.as_json,
-          "name"         => cohort.name.as_json,
-          "location_id"  => cohort.location_id.as_json,
-          "in_session"   => cohort.in_session.as_json,
-          "start_date"   => cohort.start_date.as_json,
-          "email"        => cohort.email.as_json,
-          "visible"      => cohort.visible.as_json,
-          "slug"         => cohort.slug.as_json,
-        )
+    describe "PUT /v1/cohorts/:id" do
+      it "should render unauthorized" do
+        put "v1/cohorts/#{cohort.id}", cohort: {name: "clown pajams"}
+        expect(response.status).to eq 401
+        expect(response.json).to eq("errors"=>["Unauthorized"], "status"=>401)
+      end
+    end
+
+
+    describe "DELETE /v1/users/:id" do
+      it "should render unauthorized" do
+        delete "/v1/cohorts/#{cohort.id}"
+        expect(response.status).to eq 401
+        expect(response.json).to eq("errors"=>["Unauthorized"], "status"=>401)
+      end
+    end
+
+  end
+
+
+
+  as_an :editor do
+
+    describe 'GET /v1/cohorts' do
+      it "should render all cohorts as json" do
+        get '/v1/cohorts'
+        expect( response ).to be_ok
+        expect( response.body ).to eq dbc.cohorts.all.to_json
+      end
+    end
+
+    describe 'POST /v1/cohorts' do
+      it "should render unauthorized" do
+        post '/v1/cohorts', cohort: {name: "clown pajams"}
+        expect(response.status).to eq 401
+        expect(response.json).to eq("errors"=>["Unauthorized"], "status"=>401)
+      end
+    end
+
+
+    describe "GET /v1/cohorts/:id" do
+      it "should render the cohort as json" do
+        get "v1/cohorts/#{cohort.id}"
+        expect(response.json).to eq serialize(cohort)
       end
     end
 
